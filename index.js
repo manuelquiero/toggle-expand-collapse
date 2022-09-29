060744d56cb0fda8e608a9def5a92272c47d9f9a
Hanz Jonathan Manuel <hanzjmanuel@gmail.com>
2022-09-29 23:48:36 +0800

    working

---
+++ index.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('.toggle-btn').forEach(i => {
+    i.addEventListener('click', (e) => {
+        const isExpanded = e.target.dataset['expand'] === '1';
+        const content = e.target.nextElementSibling;
+        const iconElem = e.target.children[1];
+
+        if(isExpanded){
+            //collapse
+            iconElem.innerHTML = '&plus;';
+            content.style.display = 'none';
+        } else {
+            //expand
+            iconElem.innerHTML = '&minus;';
+            content.style.display = 'block';
+        }
+        e.target.dataset['expand'] = isExpanded ? '0' : '1';
+    });
+});
